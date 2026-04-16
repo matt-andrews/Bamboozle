f6447a3d77b14a79495a1e8a3cd536236073fb05
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2026-04-15 22:28:43 -0400

    Add project files.

---
+++ Bamboozle.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Bamboozle.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Bamboozle/Controllers/ControlController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bamboozle.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ControlController : ControllerBase
+{
+    [HttpPost("routes")]
+    public IActionResult PostRoutes()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("routes/{id}")]
+    public IActionResult PutRoutes(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("routes")]
+    public IActionResult DeleteRoutes()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("routes")]
+    public IActionResult GetRoutes()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("routes/{id}/calls")]
+    public IActionResult GetRouteCalls(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("routes/{id}/calls")]
+    public IActionResult DeleteRouteCalls(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("routes/{id}/assert")]
+    public IActionResult Assert(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("unmatched")]
+    public IActionResult GetUnmatched()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("reset")]
+    public IActionResult Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("health")]
+    public IActionResult Health()
+    {
+        throw new NotImplementedException();
+    }
+}
