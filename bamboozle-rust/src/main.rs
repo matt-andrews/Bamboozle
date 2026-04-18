9459e4cd162521e0497f24acdbae98489b0d4d41
Matthew Andrews <Matthew.Andrews.018@gmail.com>
2026-04-18 02:01:37 -0400

    fix logging

--- bamboozle-rust/src/main.rs
+++ bamboozle-rust/src/main.rs
@@ -14,7 +14,13 @@ use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .with_ansi(false)
+        .init();
 
     let config = config::AppConfig::from_env()?;
     let state = app_state::AppState::new();
